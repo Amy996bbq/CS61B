3d7d2d17d69a5d54abd3aa310e829a22b7c8f7f1
AnMeiyi <meiyian996@gmail.com>
2020-09-22 14:57:03 +0800

    loading first time

---
+++ Learn_CS61B/src/Lab0/Max.java
@@ -0,0 +1,18 @@
+package Lab0;
+
+public class Max {
+    public static int max(int[] m) {
+        int max = m[0];
+        for(int i = 1; i < m.length; i++) {
+            if(max < m[i]) {
+                max = m[i];
+            }
+        }
+        return max;
+    }
+    public static void main(String[] args) {
+        int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+        int max_numbers = max(numbers);
+        System.out.println(max_numbers);
+    }
+}
